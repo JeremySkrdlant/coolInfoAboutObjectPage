4b64f9ab98e970dd04d3c3a2d6f4f641621cfbe2
Jeremy Skrdlant <jeremy.skrdlant@nwktc.edu>
2020-09-04 14:12:17 -0600

    testing

--- main.js
+++ main.js
@@ -1,7 +1,23 @@
 let h1s = document.querySelectorAll("h1")
 var name = ""
 if (h1s.length > 0){
-  name = h1s[0].innerHTML;
+  var name = findGetParameter("studentName");
+  h1s[0].innerHTML = name;
+
   let characterfontSize = (100 / name.length) * 2;
   h1s[0].style.fontSize = characterfontSize + "vw";
+}
+
+
+function findGetParameter(parameterName) {
+    var result = null,
+        tmp = [];
+    location.search
+        .substr(1)
+        .split("&")
+        .forEach(function (item) {
+          tmp = item.split("=");
+          if (tmp[0] === parameterName) result = decodeURIComponent(tmp[1]);
+        });
+    return result;
 }
