968488d7a86b984a7bde2d44b95e8783e13929d8
Jeremy Skrdlant <jeremy.skrdlant@nwktc.edu>
2020-09-09 10:18:00 -0600

    finished entry

--- qrForm.js
+++ qrForm.js
@@ -3,11 +3,21 @@ let qrArea = document.getElementById("code");
 
 studentData.addEventListener("input",()=>{
   let text = studentData.value
-  let split = text.split("\t")
   qrArea.innerHTML = ""
-  console.log(split)
+  if(text == "" ){
+    return;
+  }
+  let split = text.split("\t")
   let urlString = "https://jeremy-tutorial-2020.s3.us-east-2.amazonaws.com/index.html"
   urlString += "?studentName=" + split[0] + ",%20" + split[1];
   urlString += "&details=" + split[2] + "%20" + split[3];
-  new QRCode(qrArea, urlString);
+  new QRCode(qrArea,
+  {
+    text: urlString,
+    width: 512,
+    height: 512,
+    colorDark : "#000000",
+    colorLight : "#ffffff",
+    correctLevel : QRCode.CorrectLevel.H
+});
 })
